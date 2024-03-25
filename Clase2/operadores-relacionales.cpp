be1efdce98f4517f48fa0874ed117e3013e7acc3
Diego Peppert <dpeppert@cfi.org.ar>
2024-03-25 17:00:51 -0300

    Ejercicio Operadores Relacionales

---
+++ Clase2/operadores-relacionales.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+//operadores relacionales. 
+
+const int VALOR = 10;
+    
+int main() { 
+
+    //Inicialización de Variables
+    bool resultado = false; 
+
+    cout <<resultado  << endl;
+
+    int ingresoPorTeclado = 0;
+    cout <<"Ingrese un resultado" << endl;
+
+    cin >> ingresoPorTeclado;
+    resultado = ingresoPorTeclado == VALOR;
+
+    cout << resultado;
+
+}
